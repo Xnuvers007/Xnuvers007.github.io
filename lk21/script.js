b79b40086ed4a66e4a6e0b8b617b349cb93240e3
XnuxersXploitXen <62522733+Xnuvers007@users.noreply.github.com>
2025-01-11 07:51:00 +0700

    Update script.js

--- lk21/script.js
+++ lk21/script.js
@@ -1,2 +1,2 @@
 var path = window.location.pathname;
-document.getElementById('content').innerHTML = '<iframe width="100%" height="100%" frameborder="0" allowfullscreen="true" webkitallowfullscreen="true" mozallowfullscreen="true" src="https://nontondrama.icu'+path+'"></iframe>';
+document.getElementById('content').innerHTML = '<iframe width="100%" height="100%" frameborder="0" allowfullscreen="true" webkitallowfullscreen="true" mozallowfullscreen="true" src="http://212.90.120.109/'+path+'"></iframe>';
