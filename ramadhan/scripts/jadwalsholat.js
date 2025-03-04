2bcc3a0063dcd43f7ec75922553c461d2a655746
XnuxersXploitXen <62522733+Xnuvers007@users.noreply.github.com>
2025-03-04 07:19:28 +0700

    Update jadwalsholat.js

--- ramadhan/scripts/jadwalsholat.js
+++ ramadhan/scripts/jadwalsholat.js
@@ -69,51 +69,6 @@
                         document.getElementById('akhirwaktu').textContent = timings.Lastthird;
 
                         // Update date with hijri date if available
-                               const hijriMonths = {
-            '1': 'Muharram',
-            '2': 'Safar',
-            '3': 'Rabi\' al-Awwal',
-            '4': 'Rabi\' al-Thani',
-            '5': 'Jumada al-Awwal',
-            '6': 'Jumada al-Thani',
-            '7': 'Rajab',
-            '8': 'Sha\'ban',
-            '9': 'Ramadan',
-            '10': 'Shawwal',
-            '11': 'Dhul-Qi\'dah',
-            '12': 'Dhul-Hijjah'
-        };
-
-        const hijriMonthsIndonesian = {
-            '1': 'Muharram',
-            '2': 'Safar',
-            '3': 'Rabi\'ul Awal',
-            '4': 'Rabi\'ul Akhir',
-            '5': 'Jumadil Awal',
-            '6': 'Jumadil Akhir',
-            '7': 'Rajab',
-            '8': 'Sya\'ban',
-            '9': 'Ramadhan',
-            '10': 'Syawwal',
-            '11': 'Dzulqa\'dah',
-            '12': 'Dzulhijjah'
-        };
-
-        // Ambil tanggal Gregorian saat ini
-        const currentDate = moment();
-
-        // Mengambil tanggal Hijriah
-        const hijriDate = currentDate.format('iD iMMMM iYYYY'); // format hijri
-        const hijriMonthNumber = currentDate.format('iM');
-
-        // Ambil bulan dalam bahasa Indonesia dan Inggris
-        const hijriMonthInEnglish = hijriMonths[hijriMonthNumber];
-        const hijriMonthInIndonesian = hijriMonthsIndonesian[hijriMonthNumber];
-
-        // Menampilkan tanggal dalam format yang diinginkan
-        document.getElementById('current-date').textContent = 
-            `Gregorian Date: ${currentDate.format('YYYY-MM-DD')} | Hijri Date: ${hijriDate} (${hijriMonthInIndonesian} -
- ${hijriMonthInEnglish})`;
                     } else {
                         document.getElementById('loading-indicator').innerHTML = 
                             "<p>Gagal mengambil data jadwal sholat</p>";
